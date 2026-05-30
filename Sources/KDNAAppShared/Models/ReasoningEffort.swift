e3d8887f22b4056d5a0b64d70bae224924c41567
AIBUBB技术团队 <driknix@gmail.com>
2026-05-30 19:11:58 +0800

    fix: make GenerationSettings init public

--- Sources/KDNAAppShared/Models/ReasoningEffort.swift
+++ Sources/KDNAAppShared/Models/ReasoningEffort.swift
@@ -86,13 +86,10 @@ public enum ReasoningEffort: String, Codable, CaseIterable, Sendable {
 }
 
 public struct GenerationSettings: Codable, Sendable, Equatable {
-    public init(temperature: Float = 0.7, reasoningEffort: ReasoningEffort = .medium) {
-        self.temperature = temperature; self.reasoningEffort = reasoningEffort
-    }
     public var temperature: Float
     public var reasoningEffort: ReasoningEffort
 
-    init(temperature: Float, reasoningEffort: ReasoningEffort = .off) {
+    public init(temperature: Float, reasoningEffort: ReasoningEffort = .off) {
         self.temperature = temperature
         self.reasoningEffort = reasoningEffort
     }
